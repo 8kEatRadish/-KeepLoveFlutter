42df6751e76853842257fb10c6044fb846772d03
suihw <18845795735@163.com>
2018-12-20 19:29:33 +0800

    feat: add fromjson

--- keep_love_flutter/lib/pages/indexPage.dart
+++ keep_love_flutter/lib/pages/indexPage.dart
@@ -1,15 +1,37 @@
 import 'package:flutter/material.dart';
 
+import '../util/dataUtils.dart';
+import '../model/indexCell.dart';
+import '../widgets/indexListCell.dart';
+
 class IndexPage extends StatefulWidget{
   _IndexPageState createState() => _IndexPageState();
 }
 
 class _IndexPageState extends State<IndexPage>{
+  List<IndexCell> _listData;
+  getList(bool isLoadMore){
+    DataUtils.getIndexListData().then((resultList){
+      setState(() {
+        _listData = resultList;
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getList(false);
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Container(
-      child: Text('IndexPage'),
+    return Column(
+      children: <Widget>[
+        Text('IndexPage'),
+        IndexListCell(cellInfo: _listData[0],)
+      ],
     );
   }
 }
---
+++ keep_love_flutter/lib/widgets/indexListCell.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import '../model/indexCell.dart';
+
+class IndexListCell extends StatelessWidget{
+  final IndexCell cellInfo;
+  IndexListCell({Key key,this.cellInfo}):super(key : key);
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      child: Text(cellInfo.username),
+    );
+  }
+}
